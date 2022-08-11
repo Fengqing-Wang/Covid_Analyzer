6fb14d6a07988ef0d7fdbd803a3f00f396eda7bf
Beaver_World <95999849+Chain1216@users.noreply.github.com>
2022-08-10 22:40:53 -0400

    Update Processor.java

--- cit594/processor/Processor.java
+++ cit594/processor/Processor.java
@@ -1,11 +1,18 @@
 package edu.upenn.cit594.processor;
 
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import edu.upenn.cit594.datamanagement.CovidComparatorr;
 import edu.upenn.cit594.datamanagement.CovidReader;
 import edu.upenn.cit594.datamanagement.PopulationReader;
 import edu.upenn.cit594.datamanagement.PropertiesReader;
+import edu.upenn.cit594.util.Covid;
 import edu.upenn.cit594.util.Population;
 
 
@@ -42,7 +49,78 @@ public class Processor {
 		for(Population p : pop) {
 			total+=p.getPopulation();
 		}
+		System.out.println("-----BEGIN OUTPUT-----");
 		System.out.println(total);
+		System.out.println("-----END OUTPUT-----");
+		System.out.println("                    ");
+		System.out.println("                    ");
+	}
+	
+	
+	/* Check the input date*/
+	public boolean checkdata(String date) {
+		List<Covid> covids;
+		if(CovidComparatorr.compare(covidReader.getFilename())) {
+			covids = covidReader.getAllcovid_csv();
+		}else {
+			covids = covidReader.getAllcovid_json();
+		}
+		
+		for(Covid c: covids) {
+			if(c.getTime().equals(date)) {
+				return true;
+			}
+		}
+		
+		return false;
+		
+	}
+	
+	/* 3. Show the total vaccinations per capita for each ZIP Code for the specified date (subsection 3.3). */
+	public void PrintVaccination(String choice, String data) {
+		
+		List<Covid> covids;
+		if(CovidComparatorr.compare(covidReader.getFilename())) {
+			covids = covidReader.getAllcovid_csv();
+		}else {
+			covids = covidReader.getAllcovid_json();
+		}
+		
+		List<Population> pop = new ArrayList<Population>();
+		
+		try {
+			pop = popReader.getAllpop();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		Map<String,Integer> population = new HashMap<String,Integer>();
+		
+		for(Population p:pop) {
+			population.put(p.getZip_code(), p.getPopulation());
+		}
+		
+		
+		System.out.println("-----BEGIN OUTPUT-----");
+		
+		if(choice.equals("full")) {
+			for(Covid c:covids) {
+				if(c.getTime().equals(data) && c.getFully_vaccinated()!=0 && population.containsKey(c.getZipCode())) {
+					System.out.println(c.getZipCode()+" "+String.format("%.4f", ((double) c.getFully_vaccinated()/(double)population.get(c.getZipCode()))));
+				}
+			}	
+		}else if (choice.equals("partial")) {
+			for(Covid c:covids) {
+				if(c.getTime().equals(data) && c.getPartially_vaccinated()!=0 && population.containsKey(c.getZipCode())) {
+					System.out.println(c.getZipCode()+" "+String.format("%.4f", ((double) c.getPartially_vaccinated()/(double)population.get(c.getZipCode()))));
+				}
+			}
+		}
+		System.out.println("-----END OUTPUT-----");
+		System.out.println("                    ");
+		System.out.println("                    ");
+		
+		
 	}
 	
 	/* methods to set Reader of input files*/
